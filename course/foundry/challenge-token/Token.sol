265a7fb2c30267b768534c60cd1344ac5dfc198b
yu1hpa <yu.1hpa@gmail.com>
2023-09-11 22:40:24 +0900

    feat: Implementation of Fungible Token Transfer

--- course/foundry/challenge-token/Token.sol
+++ course/foundry/challenge-token/Token.sol
@@ -7,12 +7,21 @@ contract Token {
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
+    event Transfer(address indexed from, address indexed to, uint256 tokens);
+
     constructor() {
         balanceOf[msg.sender] = totalSupply;
     }
 
     function transfer(address to, uint256 amount) public returns (bool success) {
-        ////////// YOUR CODE HERE //////////
+        require(to != address(0), "Invalid address");
+        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+
+        emit Transfer(msg.sender, to, amount);
+        return true;
     }
 
     function transferFrom(address from, address to, uint256 amount) public returns (bool success) {
